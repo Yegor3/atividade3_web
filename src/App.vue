7efdd3873ea1bb242eacc23f5da3ddc60afb195b
Igor Marques Aguiar <59533696+Yegor3@users.noreply.github.com>
2021-07-20 16:07:27 -0300

    arquivos principais e componentes

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Posts> </Posts>
+  </div>
+</template>
+
+<script>
+import Posts from "./components/Posts.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    Posts,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,39 @@
+<template>  
+  <div >
+  <button @click="fetchPosts" class="btn btn-success">Posts</button><br>
+
+<ul>
+
+<div v-for="post in posts" :key="post.id">
+    <div>
+      <li class="table table-dark">
+         <th scope="col">Title: </th> 
+         <th scope="col">Body: </th>    
+        <tr>
+        <th scope="col">{{post.title}}</th>
+        <th scope="col">{{post.body}}</th>
+        </tr>
+    </li>
+</div>
+</div>
+</ul>
+</div>
+</template>
+<script>
+export default {
+    name: "Posts",
+    data() {
+    return {
+     posts: [],
+     baseURI: "https://jsonplaceholder.typicode.com/posts"
+    };
+    },
+    methods: {
+    fetchPosts: function() {
+      this.$http.get(this.baseURI).then((result) => {
+      this.posts = result.data;
+    });
+    },
+    },
+};
+</script>
